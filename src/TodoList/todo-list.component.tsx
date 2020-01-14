b793dfb5768b184df0b0270e56c95ccd2812f39e
Alex Bennett <alexandergraybennett@gmail.com>
2020-01-14 10:40:59 -0700

    adds interaction

--- src/TodoList/todo-list.component.tsx
+++ src/TodoList/todo-list.component.tsx
@@ -1,5 +1,116 @@
-import React from 'react'
+import React, { SetStateAction, Dispatch } from 'react'
+
+type Todo = { id: number; name: string; complete: boolean }
+type TodoStateHook = [Todo[], Dispatch<SetStateAction<Todo[]>>]
+type InputStateHook = [string, Dispatch<SetStateAction<string>>]
+type TodoCustomHook = () => TodoStateHook
+
+export const useList: TodoCustomHook = () => {
+  const [todos, setTodos]: TodoStateHook = React.useState<Todo[]>([
+    { id: 0, name: 'Learn Testing', complete: false }
+  ])
+
+  return [todos, setTodos]
+}
+
+interface ApiHookState<T> {
+  state: T
+  loading: boolean
+}
+
+export function useApi(endpoint: string): ApiHookState<string> {
+  const [state, setState] = React.useState<string>('')
+  const [loading, setLoading] = React.useState<boolean>(true)
+
+  React.useEffect((): void => {
+    fetch(endpoint)
+      .then((response: Response): void => {
+        if (response.status === 200) {
+          response.json().then((data: { title: string }) => {
+            setState(data.title)
+            setLoading(false)
+          })
+        }
+        setLoading(false)
+      })
+      .catch((err: Error) => {
+        setLoading(false)
+      })
+  }, [endpoint])
+
+  return {
+    state,
+    loading
+  }
+}
 
 export const TodoList: React.FC = () => {
-  return <div data-testid="todos"></div>
+  const [todos, setTodos]: TodoStateHook = useList()
+  const [inputValue, setInputValue]: InputStateHook = React.useState<string>('')
+
+  return (
+    <div data-testid="todos">
+      <h3 data-testid="todos__title" className="todos__title">
+        Todo List
+      </h3>
+      <ul data-testid="todos__list">
+        {todos &&
+          todos.map(
+            (todo: Todo): JSX.Element => {
+              return (
+                <li
+                  key={`${todo.name} + ${todo.id}`}
+                  data-testid="todo__item"
+                  style={{ color: todo.complete ? '#eaeaea' : 'black' }}
+                >
+                  {todo.name}
+                  <button
+                    data-testid="todo__delete"
+                    onClick={e => {
+                      setTodos(todos.filter((newTodo: Todo): Boolean => todo.id !== newTodo.id))
+                    }}
+                  >
+                    Delete
+                  </button>
+                  <button
+                    data-testid="todo__toggle"
+                    onClick={e => {
+                      setTodos(
+                        todos.map(
+                          (currentTodo: Todo): Todo => {
+                            return todo.id === currentTodo.id
+                              ? {
+                                  ...currentTodo,
+                                  complete: !currentTodo.complete
+                                }
+                              : currentTodo
+                          }
+                        )
+                      )
+                    }}
+                  >
+                    Done
+                  </button>
+                </li>
+              )
+            }
+          )}
+      </ul>
+      <input
+        type="text"
+        value={inputValue}
+        data-testid="todo__input"
+        onChange={e => setInputValue(e.target.value)}
+      />
+      <button
+        data-testid="todo__button"
+        onClick={e => {
+          setTodos([...todos, { id: todos.length + 1, name: inputValue, complete: false }])
+          setInputValue('')
+        }}
+      >
+        Add Item
+      </button>
+    </div>
+  )
 }
--- src/TodoList/todo-list.test.tsx
+++ src/TodoList/todo-list.test.tsx
@@ -1,9 +1,84 @@
 import React from 'react'
-import { render } from '@testing-library/react'
-import { TodoList } from './todo-list.component'
+import { render, waitForElement, fireEvent } from '@testing-library/react'
+import { renderHook, act } from '@testing-library/react-hooks'
+import { TodoList, useList, useApi } from './todo-list.component'
 
 test('Should have the necessary elements', async () => {
-  const { container } = render(<TodoList />)
+  const { container, getByTestId } = render(<TodoList />)
 
-  expect(container instanceof HTMLDivElement).toBe(true) // true
+  const todosElement = getByTestId('todos')
+  const todosTitle = getByTestId('todos__title')
+  const todoInput = getByTestId('todo__input')
+
+  expect(todosElement).toBeInTheDocument()
+  expect(todosTitle.className).toBe('todos__title')
+  expect(todoInput).toBeInTheDocument()
+  expect(container).toBeInTheDocument()
+})
+
+test('Show show the list of todods', async () => {
+  const { getAllByTestId } = render(<TodoList />)
+  const todos = getAllByTestId('todo__item')
+
+  expect(todos[0].textContent).toBe('Learn TestingDeleteDone')
+})
+
+test('Should be able to add a new todo', async () => {
+  const { getByTestId, findByText } = render(<TodoList />)
+  const input = getByTestId('todo__input')
+  const button = getByTestId('todo__button')
+
+  fireEvent.change(input, { target: { value: 'take out trash' } })
+  fireEvent.click(button)
+
+  const newItem = await waitForElement(() => findByText('take out trash'))
+
+  expect(newItem.textContent).toBe('take out trashDeleteDone')
+  expect(input.value).toBe('')
+})
+
+test('Should delete an item', async () => {
+  const { getByText, getByTestId } = render(<TodoList />)
+  const deleteButton = getByText('Delete')
+
+  fireEvent.click(deleteButton)
+
+  const updatedTodos = await waitForElement(() => getByTestId('todos__list'))
+  expect(updatedTodos.children.length).toBe(0)
+})
+
+test('Should toggle complete a todo task', async () => {
+  const { getByTestId, getByText } = render(<TodoList />)
+  const itemToComplete = getByText('Learn Testing')
+
+  expect(itemToComplete.style.color).toBe('black')
+  const completeButton = getByTestId('todo__toggle')
+
+  fireEvent.click(completeButton)
+
+  const completedItem = await waitForElement(() => getByTestId('todo__item'))
+  expect(completedItem.style.color).toBe('rgb(234, 234, 234)')
+})
+
+test('hook', () => {
+  const { result } = renderHook(() => useList())
+
+  const setState = result.current[1]
+
+  act(() =>
+    setState([
+      { id: 0, name: 'Learn Testing', complete: false },
+      { id: 1, name: 'Test Hooks', complete: true }
+    ])
+  )
+
+  expect(result.current[0]).toHaveLength(2)
+})
+
+test('apihook', async () => {
+  const { result, waitForNextUpdate } = renderHook(() =>
+    useApi('https://jsonplaceholder.typicode.com/todos/2')
+  )
+
+  console.log(result.current)
 })
